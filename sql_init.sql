a6f2a89bd12e646b473920a2bcdb1393a9352597
souvikdas <Souvik.Das@dal.ca>
2020-04-06 22:41:26 -0300

    sql-init statements

---
+++ sql_init.sql
@@ -0,0 +1,25 @@
+#Table 1;
+create table orderPurchased
+(
+    OrderID     int(11) auto_increment,
+    SupplierID  int(11),
+    placedDate  Date,
+    arrivedDate Date,
+    ShipperID   int(11),
+    trackingID  varchar(20),
+    constraint pk primary key (OrderID),
+    constraint `fk_1` foreign key (`SupplierID`) references `suppliers` (`SupplierID`),
+    constraint `fk_2` foreign key (`ShipperID`) references `shippers` (`ShipperID`)
+);
+
+#Table 2:
+create table purchaseDetails
+(
+    OrderID     int(11),
+    ProductID   int(11),
+    Units int,
+    productCost decimal(10, 4),
+    constraint PK_1 PRIMARY KEY (OrderID, ProductID),
+    constraint `FK_3` foreign key (`OrderID`) references `orderPurchased` (`OrderID`),
+    constraint `FK_4` foreign key (`ProductID`) references `products` (`ProductID`)
+);
